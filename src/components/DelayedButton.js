05fb708103fc264c1068ca2fa9dad1d77811f033
Susanta Biswas <susantasky@gmail.com>
2018-11-19 12:20:16 +0000

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
-// Code DelayedButton Component Here
+// Code CoordinatesButton Component Here
+import React from 'react';
+
+export default class DelayedButton extends React.Component{
+  eventFn = (event) => {
+    setTimeout(()=>{
+      this.props.onDelayedClick(event);
+    }, this.props.delay)
+  }
+
+  render(){
+    return(
+      <button onClick={this.eventFn}>
+      </button>
+    );
+  }
+}
